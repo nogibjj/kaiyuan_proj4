dd802cdd497b79da3ab75468583bb59ea7607b93
Kaiyuan Li <35396906+Apple-Pie-smile@users.noreply.github.com>
2022-11-28 02:51:28 +0000

    fix pylint

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, HTTPException
 import uvicorn
-from fruitlib.ranfruit import fruit_generator
+#from fruitlib.ranfruit import fruit_generator
 from pydantic import BaseModel
 from typing import List
 
 app = FastAPI()
+
 
 class Song(BaseModel):
 
@@ -12,27 +13,34 @@ class Song(BaseModel):
     artist: str
     description: str
 
+
 store_songlist = []
+
 
 @app.get("/")
 async def root():
-    return {"message": "Hello Project 4 This is a song list. Add/Read/Delete/Update your songs"}
+    return {
+        "message": "Hello Project 4 This is a song list. Add/Read/Delete/Update your songs"
+    }
 
-#create
-@app.post('/add/')
+
+# create
+@app.post("/add/")
 async def create_song(song: Song):
     store_songlist.append(song)
     return song
 
-#read
-@app.get('/getall/', response_model=List[Song])
+
+# read
+@app.get("/getall/", response_model=List[Song])
 async def get_all_songs():
     return store_songlist
 
-#delete
-@app.delete('/delete/{name}')
+
+# delete
+@app.delete("/delete/{name}")
 async def delete_song(name: str):
-    for i,x in enumerate(store_songlist):
+    for i, x in enumerate(store_songlist):
         if x.name == name:
             obj = store_songlist[i]
             store_songlist.pop(i)
@@ -40,24 +48,24 @@ async def delete_song(name: str):
 
     raise HTTPException(status_code=404, detail="Song Not Found")
 
-#update
-@app.put('/update/{name}')
+
+# update
+@app.put("/update/{name}")
 async def update_todo(name: str, song: Song):
-    for i,x in enumerate(store_songlist):
+    for i, x in enumerate(store_songlist):
         if x.name == name:
             store_songlist[i] = song
             return store_songlist[i]
 
     raise HTTPException(status_code=404, detail="Song Not Found")
 
-'''
-@app.get("/fruits/{fruit}")
-async def myfruit(fruit: str):
-    """Adds a fruit to random fruit"""
 
-    chosen_random_fruit = fruit_generator(fruit)
-    return {"random_fruit": chosen_random_fruit}
-    '''
+#@app.get("/fruits/{fruit}")
+#async def myfruit(fruit: str):
+    #Adds a fruit to random fruit
 
-if __name__ == '__main__':
-    uvicorn.run(app, port=8080, host='0.0.0.0')
+#   chosen_random_fruit = fruit_generator(fruit)
+#   return {"random_fruit": chosen_random_fruit}
+
+if __name__ == "__main__":
+    uvicorn.run(app, port=8080, host="0.0.0.0")
